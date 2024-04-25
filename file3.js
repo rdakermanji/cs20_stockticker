094a144597bc75138c03dc0d285304d65adb6f3d
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-25 15:43:22 -0400

    Update file3.js

--- file3.js
+++ file3.js
@@ -1,3 +1,7 @@
+const MongoClient = require('mongodb').MongoClient;
+const connStr = "mongodb+srv://racheldakermanji:iuGimtV9NHbEAiNB@cluster0.dxi5iab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const http = require('http');
+const url = require('url');
 var port = process.env.PORT || 3000;
 
 http.createServer(function(req, res) {
