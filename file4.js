8a3a64caf0cbde0b23193c95e9723206f800448a
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-25 19:22:32 -0400

    Create file4.js

---
+++ file4.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+const http = require('http');
+
+const connStr = 'mongodb+srv://racheldakermanji:iuGimtV9NHbEAiNB@cluster0.dxi5iab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+http.createServer(async function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    MongoClient.connect(connStr, async function(err, db) {
+        if (err) {
+            res.write('Error connecting to the database');
+            res.end();
+            return;
+        }
+        //const dbo = db.
+        const collection = db.collection('PublicCompanies');
+        
+        // Here you can perform operations on the database
+        
+        res.write('Connected to the database');
+        res.end();
+    });
+}).listen(8080);
+
+console.log('Server running at http://localhost:8080/');
