8d1685462c439440338dc5436e2a8a460d33d070
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-25 18:50:27 -0400

    Update file3.js

--- file3.js
+++ file3.js
@@ -9,6 +9,7 @@ http.createServer(function(req, res) {
 	if (req.url == "/") {
 		res.write('<form method="get" action="process.js"><label for="radio1">What kind of search?</label><input type="radio" name="rad" value="ticker">Ticker </input><input type="radio" name="rad" value="name">Company Name </input><br><br><label for="str1">Enter a ticker symbol or company name:&nbsp;&nbsp;</label><input type="text" name="inp"></input><br><br><input id="submit" type="submit" value="Submit"></form>');
 		res.write("<p>RES TEST1</p>");
+		res.end();
 	} else if ((req.url).includes("/process")) {
 		res.write("<p>RES TEST on process page</p>");
 		const processquery = url.parse(req.url, true).query;
