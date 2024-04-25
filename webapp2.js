7457688f22f5ee3a4b6e59d8af24f438d7010440
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-25 01:25:10 -0400

    Update webapp2.js

--- webapp2.js
+++ webapp2.js
@@ -6,23 +6,20 @@ var qs = require('querystring');
 
 
 http.createServer(function (req, res) {
-  
+  path = req.url;
   if (path == "/")
   {
     res.write('<form method="get" action="/process"><label for="radio1">What kind of search?</label><input type="radio" name="rad" value="ticker">Ticker </input><input type="radio" name="rad" value="name">Company Name </input><br><br><label for="str1">Enter a ticker symbol or company name:&nbsp;&nbsp;</label><input type="text" name="inp"></input><br><br><input id="submit" type="submit" value="Submit"></form>';
     })
   }
-  else if (path == "/process")
-  {
-	res.write ("Processing<br/>");
-    var body = '';
-    req.on('data', chunk => { body += chunk.toString();  });
-    req.on('end', () => 
-        { 
-        res.write ("Raw data string: " + body +"<br/>");
-	    var id = qs.parse(body).id;      // assumes x is post data parameter	
-        res.write ("The id is " + id );
-        res.end();
-        });
+  else if (path.includes("/process"))
+  {	
+	var querystr = window.location.search;
+	var params = new URLSearchParams(querystr);
+	var buttontype = params.get('rad');
+	var inputvalue = params.get('inp');
+	var quer = {};
+        quer[n] = input;
+        console.log(quer);
   }
 }).listen(8080);
