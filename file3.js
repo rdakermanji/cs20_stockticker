aee5d4e4b20cb41192b25e1b2679f66ee4e6bd0a
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-25 17:43:56 -0400

    Update file3.js

--- file3.js
+++ file3.js
@@ -8,7 +8,9 @@ http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	if (req.url == "/") {
 		res.write('<form method="get" action="process.js"><label for="radio1">What kind of search?</label><input type="radio" name="rad" value="ticker">Ticker </input><input type="radio" name="rad" value="name">Company Name </input><br><br><label for="str1">Enter a ticker symbol or company name:&nbsp;&nbsp;</label><input type="text" name="inp"></input><br><br><input id="submit" type="submit" value="Submit"></form>');
+		res.write("<p>RES TEST1</p>");
 	} else if ((req.url).includes("/process")) {
+		res.write("<p>RES TEST on process page</p>");
 		const processquery = url.parse(req.url, true).query;
 		const comportick = processquery.rad;
 		/*console.log("query")
@@ -37,7 +39,7 @@ http.createServer(function(req, res) {
 							console.log("Company: " + items[i].Company + ", Ticker: " + items[i].Ticker + ", Price: " + items[i].Price);
 							res.write("Company: " + items[i].Company + ", Ticker: " + items[i].Ticker + ", Price: " + items[i].Price);
 							console.log("after res");
-							res.write("RES TEST");
+							res.write("<p>RES TEST2</p>");
 							res.end();
 						}
 					}
