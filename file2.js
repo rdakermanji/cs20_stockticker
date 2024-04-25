896bfa269cf1b08fcf93534982cc2130fc398dce
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-25 16:07:33 -0400

    Update file2.js

--- file2.js
+++ file2.js
@@ -3,14 +3,14 @@ const connStr = "mongodb+srv://racheldakermanji:iuGimtV9NHbEAiNB@cluster0.dxi5ia
 const http = require('http');
 const url = require('url');
 var port = process.env.PORT || 3000;
-console.log('here on the form page');
-/*http.createServer(function(req, res) {
+//console.log('here on the form page');
+http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	
 	if (req.url  == "/") {
 		console.log('here on the form page');
 		res.write('<form method="get" action="process.js"><label for="radio1">What kind of search?</label><input type="radio" name="rad" value="ticker">Ticker </input><input type="radio" name="rad" value="name">Company Name </input><br><br><label for="str1">Enter a ticker symbol or company name:&nbsp;&nbsp;</label><input type="text" name="inp"></input><br><br><input id="submit" type="submit" value="Submit"></form>');
-	} else if ((req.url).includes("/process")) { 
+	} else if ((req.url).includes("/process")) { /*
 		const processquery = url.parse(req.url, true).query;
 		//window.alert(querystring)
 		const comportick = processquery.rad;
@@ -36,7 +36,7 @@ console.log('here on the form page');
 				  db.close();
 				}); 		
 		}
-	} }
+	} */}
 	res.end();
 }).listen(port); 
 */
