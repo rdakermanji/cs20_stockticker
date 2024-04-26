e7b6d3a7dac9caa653850ac891b35bdae8839823
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-26 16:39:19 -0400

    Update file5.js

--- file5.js
+++ file5.js
@@ -29,7 +29,7 @@ http.createServer(function(req, res) {
 			}
 			console.log("theQuery");
 			console.log(theQuery);
-			await collection.find(theQuery).toArray(function(err, items) {
+			collection.find(theQuery).toArray(function(err, items) {
 				if (err) {
 					console.log(err);
 				} else {
