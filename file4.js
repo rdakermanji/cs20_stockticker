ced0df3893002ea2cf0ad68d485306b4d152da4e
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-26 19:52:28 -0400

    Update file4.js

--- file4.js
+++ file4.js
@@ -57,6 +57,7 @@ http.createServer(function(req, res) {
 
 							apivals(items[0].Ticker).then(result => {
 								//console.log('result' + result);
+								console.log(comp + ' ' + tick1);
 								console.log("API VALS-> Company: " + comp + ", Ticker: " + tick1 + ", Price: " + result);
 								res.write("API VALS-> Company: " + comp + ", Ticker: " + tick1 + ", Price: " + result);
 								res.write("<br>");
