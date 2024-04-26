c35c9d679ebfa314f08c14ac57cea17f9ad75daa
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-26 19:15:47 -0400

    Update file4.js

--- file4.js
+++ file4.js
@@ -42,11 +42,10 @@ http.createServer(function(req, res) {
 							var ticker = items[0].Ticker; var comp = items[i].Company; 
 							var final = apiurl + ticker + apiurl_end;
 							console.log('final' + final);
-							var v = 0;
 							res1 = fetch(final)
 							.then (res1 => res1.text())
 							.then (data => {
-							    var d1 = JSON.parse(data); var r = d1['results']; v = r[0]; v = v['c'];
+							    var d1 = JSON.parse(data); var r = d1['results']; var v = r[0]; v = v['c'];
 								console.log('v' + v);
 								console.log("Company: " + comp + ", Ticker: " + ticker + ", Price: " + v);
 							    //console.log("Company: " + items[i].Company + ", Ticker: " + items[i].Ticker + ", Price: " + v);
