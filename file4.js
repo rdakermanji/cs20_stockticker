c52ced534b7469eeb63357c7666406ad2e206096
rdakermanji <157336489+rdakermanji@users.noreply.github.com>
2024-04-26 20:47:58 -0400

    Update file4.js

--- file4.js
+++ file4.js
@@ -82,22 +82,22 @@ http.createServer(function(req, res) {
 					if (err) {
 						console.log(err);
 					} else {
-						for (i = 0; i < items.length; i++) {							
+						for (i = 0; i < items.length; i++) {
+							var comp = items[i].Company; var tick1 = items[i].Ticker;
 							console.log("Company: " + items[i].Company + ", Ticker: " + items[i].Ticker + ", Price: " + items[i].Price);
 							res.write("Company: " + items[i].Company + ", Ticker: " + items[i].Ticker + ", Price: " + items[i].Price);
 							res.write("<br>");
 							res.write("<script language=javascript>console.log('Company: " + items[i].Company + ", Ticker: " + items[i].Ticker + ", Price: " + items[i].Price + "'); </script>");
 							//response.Write("<script language=javascript>console.log(`'" & value & "'`); </script>")
 							
-							var comp = items[i].Company; var tick1 = items[i].Ticker;
-
 							apivals(items[i].Ticker).then(result => {
 								console.log("API VALS-> Company: " + comp + ", Ticker: " + tick1 + ", Price: " + result);
 								res.write("API VALS-> Company: " + comp + ", Ticker: " + tick1 + ", Price: " + result);
 								res.write("<br>");
 								res.write("<script language=javascript>console.log('API VALS-> Company: " + comp + ", Ticker: " + tick1 + ", Price: " + result + "'); </script>");
 								//res.end();
-							})						}
+							})						
+						}
 					}
 					db.close();
 				//res.end();
